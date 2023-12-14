f7970be4f1320d4f16c2d91df688698c89bda225
Duong Quan <duongquan199999@gmail.com>
2023-12-14 11:26:51 +0700

    BASIC

---
+++ src/controller/users.controller.js
@@ -0,0 +1,51 @@
+
+import usersModel from "../model/users.model";
+export default {
+    createUser: async (req,res)=>{
+        try{
+            const {name,email,age}= req.body;
+            const newUser = await usersModel.createUser({name,email,age});
+            return res.status(200).json(newUser);
+        }catch (err){
+            res.status(500).json({err:"Server fail"})
+        }
+    },
+    getAllUsers:async (req,res) => {
+        try{
+            const users=await usersModel.getAllUsers();
+            return res.status(200).json(users);
+        }catch(err){
+            res.status(500).json({err:"Server fail"})
+        }
+    },
+    getIdUsers: async (req,res)=>{
+        try{
+            const userId= parseInt(req.params.id);
+            
+            const user =await usersModel.getIdUsers(userId);
+            
+            return res.status(200).json(user);
+        } catch(err){
+            res.status(500).json({err: "Server fail"})
+        }
+    },
+    updateUser:async (req,res)=>{
+        try{
+            const userId =parseInt(req.params.id)
+            const {name,email,age}=req.body;
+const update= await usersModel.updateUser(userId,{name,email,age})
+            return res.status(200).json(update);
+        }catch(err){
+            res.status(500).json({err: "Server fail"})
+        }
+    },
+    deleteUser: async (req,res)=>{
+        try{
+            const userId = parseInt(req.params.id);
+            await usersModel.deleteUser(userId);
+            res.json({message:`Delete User ID :${userId} success`})
+        } catch(err){
+              res.status(500).json({err:"Server fail"})
+        }
+    }
+}
---
+++ src/model/users.model.js
@@ -0,0 +1,64 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+export default {
+    createUser: async ({ name, email, age }) => {
+        try {
+            return await prisma.users.create({
+                data: {
+                    name,
+                    email,
+                    age
+                }
+            })
+        } catch (err) {
+            throw new Error(`Creating error  ${err.message}`)
+        }
+    },
+
+    getAllUsers: async () => {
+        try {
+            return await prisma.users.findMany();
+        } catch (err) {
+            throw new Error(`Search error ${err.message}`)
+        }
+    },
+    getIdUsers: async (id) => {
+        try {
+            return await prisma.users.findUnique({
+                where: {
+                    id,
+                }
+            })
+        } catch (err) {
+            throw new Error(` ID error ${err.message}`)
+        }
+    },
+    updateUser: async (id, { name, email, age }) => {
+        try {
+            return await prisma.users.update({
+                where: {
+                    id,
+                },
+                data: {
+                    name,
+                    email,
+                    age
+                }
+            })
+        } catch (err) {
+            throw new Error(`Update error:${err.message}`);
+        }
+    },
+    deleteUser: async (id) => {
+        try {
+            return await prisma.users.delete({
+                where: {
+                    id,
+                }
+            });
+        } catch (err) {
+            throw new Error(`Delete Error: ${err.message}`)
+        }
+    }
+
+}
